ef2af43d22c6351f674a2a5a1c2ba529818a4207
Surendra Panday <surendra.panday@humanservices.gov.au>
2019-08-22 14:23:17 +1000

    all front end and backend change till now..

---
+++ NTBargainHunter/Controllers/UserAccountController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+/*
+ * All the login stamina will go through this
+ * @author : Surendra Panday
+ * **/
+namespace NTBargainHunter.Views.Login
+{
+   //[Route("Login")]
+    public class UserAccountController : Controller
+    {
+      // [Route("")]
+        [Route("Login")]
+        //[Route("~/")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [Route("Login")]
+        [HttpPost]
+
+        public IActionResult Login(string username, string password)
+        {
+            if (username !=null && password !=null && username.Equals("surendra") && password.Equals("123"))
+            {
+                HttpContext.Session.SetString("username", username);
+                return View("Login");
+            } else
+            {
+                ViewBag.error = "Invalid Account";
+                return View("Login");
+            }
+        }
+
+        [Route("Logout")]
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("username");
+            return RedirectToAction("Index");
+        }
+    }
+}
